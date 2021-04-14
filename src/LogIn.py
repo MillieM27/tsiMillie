cbd7aa66da93322f29c0aeb3ed4e21e210271316
“Millie <2310903m@student.gla.ac.uk>
2021-04-14 21:03:43 +0100

    Basic changes to formatting and a change to phone book from email - Next i will begin the task of testing

--- src/LogIn.py
+++ src/LogIn.py
@@ -14,12 +14,12 @@ class LogIn:
         return password
 
     def logIn(self):
-        phoneNumber = input("Enter your mobile number")
+        phoneNumber = input("Enter your mobile number ")
         password = self.getPassword(phoneNumber)
         if password == "":
             print("You are not a user")
         else:
-            if input("Enter password") == password:
-                print("You are logged in")
+            if input("Enter password ") == password:
+                print("Welcome", phoneNumber, "- You are logged in")
             else:
-                print("Wrong password, no second chances")
+                print("Incorrect Password - the account has been locked")
